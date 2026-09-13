23b1707bce264e6e7ac90ff216b6afdbe1c12a0e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-13 10:13:22 +0000

    dynobase 3.0.3

--- Casks/d/dynobase.rb
+++ Casks/d/dynobase.rb
@@ -1,9 +1,9 @@
 cask "dynobase" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.0.2,260912yvpkpp635"
-  sha256 arm:   "be5008a895eae2641bbf63257e4b27586b5660d49b77cfb90b1191bdda67c689",
-         intel: "518370cd9b6ed659af06394e3168387dd6ba7d02ab100b9669746a9c6ce1aaca"
+  version "3.0.3,2609132mr69dngf"
+  sha256 arm:   "c0f575a33a34dc470441fc308c6dc6cd513f5d1ab4ee0eb72df4d7f727e8ecf6",
+         intel: "64951971682dea0209d5cbb9fbadc4d50346d566d16118f13f4af97fe12d7019"
 
   url "https://download.todesktop.com/220811zswf4aj4x/Dynobase%20#{version.csv.first}%20-%20Build%20#{version.csv.second}-#{arch}.dmg"
   name "Dynobase"
